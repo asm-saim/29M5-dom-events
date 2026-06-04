515d36f8768b22b22bd243e00f63b279931b6c1a
asm-saim <saim777m@gmail.com>
2026-06-04 17:25:32 +0600

    addEventListener used

--- js/dom-events.js
+++ js/dom-events.js
@@ -2,3 +2,16 @@
 function blue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+// for click-id
+const clickValue3 = document.getElementById('click-id');
+clickValue3.onclick = function click3() {
+    document.body.style.backgroundColor = 'lightBlue';
+}
+
+// for make-purple, sight different form upper one.
+const makePurple = document.getElementById('make-purple');
+makePurple.onclick = purple;
+function purple() {
+    document.body.style.backgroundColor = 'purple';
+}
